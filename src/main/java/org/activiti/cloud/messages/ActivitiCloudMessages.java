ce065fc7c85b42eba92289167387227210e1b76b
Igor Dianov <igdianov@users.noreply.github.com>
2020-01-13 20:52:37 -0800

    feat(AAE-896): Cloud Native Bpmn Messages Example (#1)
    * feat: Added initial project stubs and configs
    
    * feat: added activiti-cloud-starter-messages dependency
    
    * feat: Added Helm chart
    
    * feat: Added ActivitiCloudMessages Spring Boot App
    
    * fix: Jenkins error
    
    * fix: Let's publish preview Docker image
    
    * feat: Added ActivitiCloudMessagesTest
    
    * fix(version): update activiti-cloud-messages-service to 7.1.0-PR-1-SNAPSHOT
    
    * fix: Use SNAPSHOT qualifier for preview
    
    * fix: Added activiti-snapshots repository
    
    * fix: Added build number to preview version
    
    * fix(versions): update messages-service to 7.1.0-PR-1-30-SNAPSHOT
    
    * feat: publish preview chart into chart museum
    
    * fix(Jenkinsfile): Added missing CHARTMUSEUM_CREDS
    
    * fix(chart): enable Postgres database by default
    
    * fix(version): update messages-service to 7.1.0-PR-1-32-SNAPSHOT
    
    * fix(version): Deploy snapshot version to Nexus
    
    * fix(maven-dind): Enabled jenkins-x-docker-registry mirror
    
    * feat(chart): Added rabbitmq init container
    
    * fix(Makefile): Removed updatebot make push-version
    
    * fix(version): update -messages-service to 7.1.0-PR-1-33-SNAPSHOT
    
    * fix(chart): Polish Posgresql init container

---
+++ src/main/java/org/activiti/cloud/messages/ActivitiCloudMessages.java
@@ -0,0 +1,13 @@
+package org.activiti.cloud.messages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiCloudMessages {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivitiCloudMessages.class, args);
+    }
+    
+}
---
+++ src/test/java/org/activiti/cloud/messages/ActivitiCloudMessagesTest.java
@@ -0,0 +1,17 @@
+package org.activiti.cloud.messages;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ActivitiCloudMessagesTest {
+
+    @Test
+    public void contextLoads() {
+        // success
+    }
+
+}
